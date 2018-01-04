ed2e5bef9fd7063640f07175b69a35462785b83d
jsbry <jsbry.biz@gmail.com>
2018-01-04 22:13:28 +0900

    tabs element ui

--- js/db.js
+++ js/db.js
@@ -9,7 +9,18 @@ new Vue({
 		db_charset: "",
 		viewable: {
 			dbname: true
-		}
+		},
+		editableTabsValue: '2',
+		editableTabs: [{
+			title: 'Tab 1',
+			name: '1',
+			content: 'Tab 1 content'
+		}, {
+			title: 'Tab 2',
+			name: '2',
+			content: 'Tab 2 content'
+		}],
+		tabIndex: 2
 	},
 	created: function(){
 		var self = this;
@@ -27,10 +38,57 @@ new Vue({
 			axios.get(
 				"/dbinfo/" + self.id,{
 			}).then(function(res){
-				console.log(res.data);
+				if(res.data.code == 200){
+					self.updated = res.data.result.updated;
+					self.db_name = res.data.result.db_name;
+					self.db_engine = res.data.result.db_engine;
+					self.db_charset = res.data.result.db_charset;
+				}else{
+					networkConnectError();
+				}
 			}).catch(function(error){
-				console.log(error);
+				networkConnectError();
 			});
 		},
+		networkConnectError: function(){
+			var self = this;
+			self.$alert('通信エラーが発生しました', 'エラー', {
+				confirmButtonText: 'OK',
+				callback: action => {
+					self.$message({
+						type: 'warning',
+						message: `しばらく時間をおいてから再度お試しください`
+					});
+				}
+			});
+		},
+		handleTabsEdit(targetName, action) {
+			if (action === 'add') {
+				let newTabName = ++this.tabIndex + '';
+				this.editableTabs.push({
+					title: 'New Tab',
+					name: newTabName,
+					content: 'New Tab content'
+				});
+				this.editableTabsValue = newTabName;
+			}
+			if (action === 'remove') {
+				let tabs = this.editableTabs;
+				let activeName = this.editableTabsValue;
+				if (activeName === targetName) {
+					tabs.forEach((tab, index) => {
+						if (tab.name === targetName) {
+							let nextTab = tabs[index + 1] || tabs[index - 1];
+							if (nextTab) {
+								activeName = nextTab.name;
+							}
+						}
+					});
+				}
+
+				this.editableTabsValue = activeName;
+				this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+			}
+		}
 	}
 });
--- js/project.js
+++ js/project.js
@@ -16,14 +16,30 @@ new Vue({
 			}).then(function(res){
 				if(res.data.code == 200){
 					self.projectlist = res.data.result.projectlist;
+				}else{
+					networkConnectError();
 				}
-				console.log(res.data);
 			}).catch(function(error){
-				console.log(error);
+				networkConnectError();
+			});
+		},
+		networkConnectError: function(){
+			var self = this;
+			self.$alert('通信エラーが発生しました', 'エラー', {
+				confirmButtonText: 'OK',
+				callback: action => {
+					self.$message({
+						type: 'warning',
+						message: `しばらく時間をおいてから再度お試しください`
+					});
+				}
 			});
 		},
 		locationDB: function(id) {
 			location.href = "/db/" + id;
+		},
+		locationAPI: function(id) {
+			location.href = "/api/" + id;
 		}
 	}
 });
